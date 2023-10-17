b49982014a1182cf929caf3ed92d42fb3d41830d
aamoos <aamoos@naver.com>
2023-10-18 00:19:29 +0900

    차트 컴포넌트 플러그인, 모둘 제작

---
+++ src/plugins/ChatPlugin.js
@@ -0,0 +1,14 @@
+import Chart from 'chart.js';
+
+export default {
+    install(Vue) {
+        Vue.prototype.$_Chart = Chart;
+
+        //BarChart.vue
+        //this.$_Chart
+
+        //LineChart.vue
+        //this.$_Chart
+
+    }
+}
